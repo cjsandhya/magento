f7e4778c7933042d8a29fcd67774299729be271f
cjsandhya <cjsandhya@gmail.com>
2014-06-16 14:42:59 +0530

    git pull

---
+++ countrange.php
@@ -0,0 +1,11 @@
+<?php
+  $fruit = array("orange", "pineapple", "peach", "apple", "pear", "cherry");
+
+  sort($fruit);
+  print_r($fruit);
+
+  echo count($fruit);
+
+  $range_array = range(0, 50, 5);
+  print_r($range_array);
+?>
