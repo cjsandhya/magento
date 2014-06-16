98992e01e1fb27e74b84aa22bb2784eeabde6eab
cjsandhya <cjsandhya@gmail.com>
2014-06-16 15:04:30 +0530

    this is multi dimen array

---
+++ multidimenarray.php
@@ -0,0 +1,8 @@
+<?php
+
+//$arr=array('apple','orange','pear');
+$arr1=array("fruits" => array("a" => "apple","b" => "pear"),"numbers"=>array(1,2,3),"holes"=>array("first","second"));
+echo $arr1["fruits"]["b"];
+
+
+?>
